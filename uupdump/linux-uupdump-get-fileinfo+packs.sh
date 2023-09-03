330f78b285f5626307f10cc2d02b3e864a53836a
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-09-03 14:46:36 +0200

    Update linux-uupdump-get-fileinfo+packs.sh

--- uupdump/linux-uupdump-get-fileinfo+packs.sh
+++ uupdump/linux-uupdump-get-fileinfo+packs.sh
@@ -4,29 +4,27 @@ pushd ./uup && rm -r uuptmp 2>/dev/null
 
 echo Client
 $fetchdetect All Retail Mainline 17763 1 125
-$fetchdetect All Retail Mainline 19044 1
+$fetchdetect All Retail Mainline 19044 1 125
 $fetchdetect All Retail Mainline 19045 1
 $fetchdetect All Retail Mainline 22000 1
 $fetchdetect All Retail Mainline 22621 1
 
-$fetchdetect All ReleasePreview Mainline 19044 1
 $fetchdetect All ReleasePreview Mainline 19045 1
 $fetchdetect All ReleasePreview Mainline 22000 1
 $fetchdetect All ReleasePreview Mainline 22621 1
 
+$fetchdetect All Beta Mainline 22000 1
 $fetchdetect All Beta Mainline 22621 1
-$fetchdetect All Beta Mainline 22624 1
+$fetchdetect All Beta Mainline 22631 1
 
-$fetchdetect All Dev Mainline 23403 1001
-$fetchdetect All Canary Mainline 25314 1000
+$fetchdetect All Dev Mainline 22621 1
+$fetchdetect All Canary Mainline Latest
 echo ""
 
 echo Server
 $fetchdetect All Retail Mainline 20348 1 8
 $fetchdetect All Retail Mainline 20348 1 406
 $fetchdetect All Retail Mainline 20349 1 406
-
-$fetchdetect All ReleasePreview Mainline 20348 1 8
-$fetchdetect All ReleasePreview Mainline 20348 1 406
-$fetchdetect All ReleasePreview Mainline 20349 1 406
+$fetchdetect All Retail Mainline 25398 1 8
+$fetchdetect All Retail Mainline 25398 1 406
 echo ""
