fee15cffe202d7eae325c109a954be0982f32a32
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-09-03 14:55:01 +0200

    Update linux-uupdump-get-fileinfo+packs.sh

--- uupdump/linux-uupdump-get-fileinfo+packs.sh
+++ uupdump/linux-uupdump-get-fileinfo+packs.sh
@@ -3,28 +3,32 @@ fetchdetect="php ./sta/fetchdetect.php"
 pushd ./uup && rm -r uuptmp 2>/dev/null
 
 echo Client
-$fetchdetect All Retail Mainline 17763 1 125
-$fetchdetect All Retail Mainline 19044 1 125
-$fetchdetect All Retail Mainline 19045 1
-$fetchdetect All Retail Mainline 22000 1
-$fetchdetect All Retail Mainline 22621 1
+$fetchdetect all Retail Mainline 17763 1 125
+$fetchdetect amd64 Retail Mainline 19042 1 119
+$fetchdetect all Retail Mainline 19044 1 125
+$fetchdetect all Retail Mainline 19045 1
+$fetchdetect all Retail Mainline 22000 1
+$fetchdetect all Retail Mainline 22621 1
 
-$fetchdetect All ReleasePreview Mainline 19045 1
-$fetchdetect All ReleasePreview Mainline 22000 1
-$fetchdetect All ReleasePreview Mainline 22621 1
+$fetchdetect all ReleasePreview Mainline 19045 1
+$fetchdetect all ReleasePreview Mainline 22000 1
+$fetchdetect all ReleasePreview Mainline 22621 1
 
-$fetchdetect All Beta Mainline 22000 1
-$fetchdetect All Beta Mainline 22621 1
-$fetchdetect All Beta Mainline 22631 1
+$fetchdetect all Beta Mainline 22000 1
+$fetchdetect all Beta Mainline 22621 1
+$fetchdetect all Beta Mainline 22631 1
 
-$fetchdetect All Dev Mainline 22621 1
-$fetchdetect All Canary Mainline Latest
+$fetchdetect amd64 Dev Mainline 19100 1 119
+$fetchdetect all Dev Mainline 22621 1
+$fetchdetect all Canary Mainline Latest
 echo ""
 
 echo Server
-$fetchdetect All Retail Mainline 20348 1 8
-$fetchdetect All Retail Mainline 20348 1 406
-$fetchdetect All Retail Mainline 20349 1 406
-$fetchdetect All Retail Mainline 25398 1 8
-$fetchdetect All Retail Mainline 25398 1 406
+$fetchdetect amd64 Retail Mainline 20348 1 8
+$fetchdetect amd64 Retail Mainline 20348 1 406
+$fetchdetect amd64 Retail Mainline 20349 1 406
+$fetchdetect amd64 Retail Mainline 25398 1 8
+$fetchdetect amd64 Retail Mainline 25398 1 406
+
+$fetchdetect amd64 ReleasePreview Mainline 20349 1 406
 echo ""
