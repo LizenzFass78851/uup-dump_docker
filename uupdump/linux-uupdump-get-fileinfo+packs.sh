65053aa3625399c88da28d0eb47c374faa1e1745
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-09-18 15:49:43 +0200

    Update linux-uupdump-get-fileinfo+packs.sh

--- uupdump/linux-uupdump-get-fileinfo+packs.sh
+++ uupdump/linux-uupdump-get-fileinfo+packs.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
-fetchdetect="php ./sta/fetchdetect.php"
+fetchupd="php ./sta/fetchupd.php"
+packsgen="php ./sta/packsgen.php"
+
 pushd ./uup && rm -r uuptmp 2>/dev/null
 
+$packsgen
+echo ""
+
 # Client builds
-$fetchdetect all Retail Mainline 17763 1 125
-$fetchdetect amd64 Retail Mainline 19042 1 119
-$fetchdetect all Retail Mainline 19044 1 125
-$fetchdetect all Retail Mainline 19045 1
-$fetchdetect all Retail Mainline 22000 1
-$fetchdetect all Retail Mainline 22621 1
+$fetchupd all Retail Mainline 17763 1 125
+$fetchupd amd64 Retail Mainline 19042 1 119
+$fetchupd all Retail Mainline 19044 1 125
+$fetchupd all Retail Mainline 19045 1
+$fetchupd all Retail Mainline 22000 1
+$fetchupd all Retail Mainline 22621 1
 
-$fetchdetect all ReleasePreview Mainline 19045 1
-$fetchdetect all ReleasePreview Mainline 22000 1
-$fetchdetect all ReleasePreview Mainline 22621 1
+$fetchupd all ReleasePreview Mainline 19045 1
+$fetchupd all ReleasePreview Mainline 22000 1
+$fetchupd all ReleasePreview Mainline 22621 1
 
-$fetchdetect all Beta Mainline 22000 1
-$fetchdetect all Beta Mainline 22621 1
-$fetchdetect all Beta Mainline 22631 1
+$fetchupd all Beta Mainline 22000 1
+$fetchupd all Beta Mainline 22621 1
+$fetchupd all Beta Mainline 22631 1
 
-$fetchdetect amd64 Dev Mainline 19100 1 119
-$fetchdetect all Dev Mainline 22621 1
-$fetchdetect all Canary Mainline Latest
+$fetchupd amd64 Dev Mainline 19100 1 119
+$fetchupd all Dev Mainline 22621 1
+$fetchupd all Canary Mainline Latest
 
 # Server builds
-$fetchdetect amd64 Retail Mainline 20348 1 8
-$fetchdetect amd64 Retail Mainline 20348 1 406
-$fetchdetect amd64 Retail Mainline 20349 1 406
-$fetchdetect amd64 Retail Mainline 25398 1 8
-$fetchdetect amd64 Retail Mainline 25398 1 406
+$fetchupd amd64 Retail Mainline 20348 1 8
+$fetchupd amd64 Retail Mainline 20348 1 406
+$fetchupd amd64 Retail Mainline 20349 1 406
+$fetchupd amd64 Retail Mainline 25398 1 8
+$fetchupd amd64 Retail Mainline 25398 1 406
 
-$fetchdetect amd64 ReleasePreview Mainline 20349 1 406
+$fetchupd amd64 ReleasePreview Mainline 20349 1 406
+echo ""
+
+rm -r uuptmp 2>/dev/null
+$packsgen
 
 echo ""
 echo Done.
