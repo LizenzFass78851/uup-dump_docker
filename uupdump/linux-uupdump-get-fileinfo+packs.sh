ee68708ce7079b44a5fb4f8bf7492ddefc192ec4
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-10-05 13:40:13 +0200

    Update linux-uupdump-get-fileinfo+packs.sh

--- uupdump/linux-uupdump-get-fileinfo+packs.sh
+++ uupdump/linux-uupdump-get-fileinfo+packs.sh
@@ -7,34 +7,42 @@ pushd ./uup && rm -r uuptmp 2>/dev/null
 $packsgen
 echo ""
 
-# Client builds
-$fetchupd all Retail Mainline 17763 1 125
-$fetchupd amd64 Retail Mainline 19042 1 119
-$fetchupd all Retail Mainline 19044 1 125
-$fetchupd all Retail Mainline 19045 1
-$fetchupd all Retail Mainline 22000 1
-$fetchupd all Retail Mainline 22621 1
-
-$fetchupd all ReleasePreview Mainline 19045 1
-$fetchupd all ReleasePreview Mainline 22000 1
-$fetchupd all ReleasePreview Mainline 22621 1
-
-$fetchupd all Beta Mainline 22000 1
-$fetchupd all Beta Mainline 22621 1
-$fetchupd all Beta Mainline 22631 1
-
-$fetchupd amd64 Dev Mainline 19100 1 119
-$fetchupd all Dev Mainline 22621 1
-$fetchupd all Canary Mainline Latest
-
+## Client builds
+#$fetchupd all Retail Mainline 17763 1 125
+#$fetchupd amd64 Retail Mainline 19042 1 119
+#$fetchupd all Retail Mainline 19044 1 125
+#$fetchupd all Retail Mainline 19045 1
+#$fetchupd all Retail Mainline 22000 1
+#$fetchupd all Retail Mainline 22621 1
+#
+#$fetchupd all ReleasePreview Mainline 19045 1
+#$fetchupd all ReleasePreview Mainline 22000 1
+#$fetchupd all ReleasePreview Mainline 22621 1
+#
+#$fetchupd all Beta Mainline 22000 1
+#$fetchupd all Beta Mainline 22621 1
+#$fetchupd all Beta Mainline 22631 1
+#
+#$fetchupd amd64 Dev Mainline 19100 1 119
+#$fetchupd all Dev Mainline 22621 1
+#$fetchupd all Canary Mainline Latest
+#
 # Server builds
-$fetchupd amd64 Retail Mainline 20348 1 8
-$fetchupd amd64 Retail Mainline 20348 1 406
-$fetchupd amd64 Retail Mainline 20349 1 406
-$fetchupd amd64 Retail Mainline 25398 1 8
-$fetchupd amd64 Retail Mainline 25398 1 406
+#$fetchupd amd64 Retail Mainline 20348 1 8
+#$fetchupd amd64 Retail Mainline 20348 1 406
+#$fetchupd amd64 Retail Mainline 20349 1 406
+#$fetchupd amd64 Retail Mainline 25398 1 8
+#$fetchupd amd64 Retail Mainline 25398 1 406
+#
+#$fetchupd amd64 ReleasePreview Mainline 20349 1 406
 
-$fetchupd amd64 ReleasePreview Mainline 20349 1 406
+fileinfoandpacksrepo="https://github.com/uup-dump"
+packs="packs.git"
+fileinfo="fileinfo.git"
+git clone $fileinfoandpacksrepo/$packs packs
+git clone $fileinfoandpacksrepo/$fileinfo fileinfo
+
+
 echo ""
 
 rm -r uuptmp 2>/dev/null
