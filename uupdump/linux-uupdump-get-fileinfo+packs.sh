e5b73f0b21fc8dd85bb8be30eda174516cd58afc
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-01-21 08:47:56 +0100

    Update linux-uupdump-get-fileinfo+packs.sh

--- uupdump/linux-uupdump-get-fileinfo+packs.sh
+++ uupdump/linux-uupdump-get-fileinfo+packs.sh
@@ -10,7 +10,7 @@ echo ""
 # Client builds
 $fetchupd all Retail Mainline 22631 1
 $fetchupd all Retail Mainline 22621 1
-$fetchupd all Retail Mainline 22000 1
+$fetchupd all Retail Mainline 22000 1 4
 $fetchupd all Retail Mainline 19045 1806
 $fetchupd all Retail Mainline 19044 1202 125
 $fetchupd amd64 Retail Mainline 19042 572 119
@@ -18,52 +18,49 @@ $fetchupd all Retail Mainline 17763 529 19
 
 $fetchupd all ReleasePreview Mainline 22631 1
 $fetchupd all ReleasePreview Mainline 22621 1
-$fetchupd all ReleasePreview Mainline 22000 1
+$fetchupd all ReleasePreview Mainline 22000 1 4
 $fetchupd all ReleasePreview Mainline 19045 1806
 $fetchupd all ReleasePreview Mainline 19044 1202 125
 
 $fetchupd all Beta Mainline 22631 1
 $fetchupd all Beta Mainline 22621 1
-$fetchupd all Beta Mainline 22000 1
+$fetchupd all Beta Mainline 22000 1 4
 
 $fetchupd all Dev Mainline 22631 1
 $fetchupd all Dev Mainline 22621 1
-
 $fetchupd amd64 Dev Mainline 19100 1019 119
-$fetchupd all Dev Mainline 19042 985 119
+$fetchupd amd64 Dev Mainline 19042 985 119
 
 $fetchupd all Canary Mainline Latest
-  
+
 # Server builds
-$fetchupd all Retail Mainline 25398 1 406
-$fetchupd all Retail Mainline 25398 1 8
-$fetchupd all Retail Mainline 20349 859 406
-$fetchupd all Retail Mainline 20348 1 406
-$fetchupd all Retail Mainline 20348 1 8
+$fetchupd amd64 Retail Mainline 25398 1 406
+$fetchupd amd64 Retail Mainline 25398 1 8
+$fetchupd amd64 Retail Mainline 20349 859 406
+$fetchupd amd64 Retail Mainline 20348 1 406
+$fetchupd amd64 Retail Mainline 20348 1 8
+$fetchupd amd64 Retail Mainline 17763 529 8
 
-$fetchupd all Retail Mainline 17763 529 8
+$fetchupd amd64 ReleasePreview Mainline 25398 287 406
+$fetchupd amd64 ReleasePreview Mainline 25398 287 408
+$fetchupd amd64 ReleasePreview Mainline 25398 287 8
+$fetchupd amd64 ReleasePreview Mainline 20349 859 406
+$fetchupd amd64 ReleasePreview Mainline 20349 825 408
+$fetchupd amd64 ReleasePreview Mainline 20348 11 406
+$fetchupd amd64 ReleasePreview Mainline 20348 1 8
+$fetchupd amd64 ReleasePreview Mainline 17763 529 8
 
-$fetchupd all ReleasePreview Mainline 25398 287 406
-$fetchupd all ReleasePreview Mainline 25398 287 408
-$fetchupd all ReleasePreview Mainline 25398 287 8
-$fetchupd all ReleasePreview Mainline 20349 859 406
-$fetchupd all ReleasePreview Mainline 20349 825 408
-$fetchupd all ReleasePreview Mainline 20348 11 406
-$fetchupd all ReleasePreview Mainline 20348 1 8
+$fetchupd amd64 Beta Mainline 25398 287 406
+$fetchupd amd64 Beta Mainline 25398 287 408
+$fetchupd amd64 Beta Mainline 25398 287 8
 
-$fetchupd all ReleasePreview Mainline 17763 529 8
+$fetchupd amd64 Dev Mainline 25398 287 406
+$fetchupd amd64 Dev Mainline 25398 287 408
+$fetchupd amd64 Dev Mainline 25398 287 8
 
-$fetchupd all Beta Mainline 25398 287 406
-$fetchupd all Beta Mainline 25398 287 408
-$fetchupd all Beta Mainline 25398 287 8
-
-$fetchupd all Dev Mainline 25398 287 406
-$fetchupd all Dev Mainline 25398 287 408
-$fetchupd all Dev Mainline 25398 287 8
-
-$fetchupd all Canary Mainline 25398 287 406
-$fetchupd all Canary Mainline 25398 287 408
-$fetchupd all Canary Mainline 25398 287 8
+$fetchupd amd64 Canary Mainline 25398 287 406
+$fetchupd amd64 Canary Mainline 25398 287 408
+$fetchupd amd64 Canary Mainline 25398 287 8
   
 
 #fileinfoandpacksrepo="https://github.com/uup-dump"
