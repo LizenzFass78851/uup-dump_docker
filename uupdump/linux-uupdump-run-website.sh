0bdca58b372ff9da4ee4c2b32225cb66378da79a
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-01-07 15:02:38 +0100

    add apache2
    - add apache2
    - change debian container to a debian-slim base image

--- uupdump/linux-uupdump-run-website.sh
+++ uupdump/linux-uupdump-run-website.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pushd ./uup && php -S 0.0.0.0:44400
+pushd ./uup # && php -S 0.0.0.0:44400
+[ -f /usr/sbin/apache2 ] && service apache2 start # for debian
+[ -f /usr/sbin/httpd ]   && httpd -k start        # for alpine
